5b625931e92f1e7c1f1a1d745cd56b336e297a2a
Frédéric van der Essen <frederic[at]vanderessen.com>
2012-04-02 00:26:38 +0200

    quick testing html file

---
+++ src/test.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	console.log("Hello World!");
+	window.canvas = document.getElementById('test_canvas_1');
+	window.context = canvas.getContext('2d');
+	context.fillStyle = 'black';
+};
